ab270b6d327babd09afa3fee9229b1015b767b70
unknown <alexbcn98@gmail.com>
2022-11-18 00:01:24 +0100

    footer updated

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.css";
 function Footer() {
-  return <div className="footer">&copy; 2022 AlexFranciscoDev All rights reserved</div>;
+  return <div className="footer">&copy; 2022 AlexFranciscoDev</div>;
 }
 
 export default Footer;
